43c8f8a4078db0e5c4d218c3e510f88afa5adc14
ichsanHPM <ichsan.helmi.ft22@mail.umy.ac.id>
2023-07-01 16:40:39 +0700

    void insert

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -25,6 +25,18 @@ public:
     {
         ROOT = Null;                        //initializing root to null
     }
+
+    void insert(string element)             //insert a node in the binary search tree
+    {
+        Node* newNode = new Node(element, NULL, NULL);              //allocate memory for the new node
+        newNode->info = element;                                    //asign value to the data field on the new node
+        newNode->leftchild = NULL;                                  //make the leftchild of the new node point to null
+        newNode->rightchild = NULL;                                 //make the right child of the new node point to null
+
+        Node* parent = NULL;
+        NOde* curentNode = NULL;
+        search(element, parent, currentNode);                       //locate the the node which will be the parent of the node to be inserted
+    }
 };
 
 int main()
