fa920f97a51d5b76590309ceedffc627e4797a03
ichsanHPM <ichsan.helmi.ft22@mail.umy.ac.id>
2023-07-02 17:47:46 +0700

    if

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -36,7 +36,22 @@ public:
         Node* parent = NULL;
         NOde* curentNode = NULL;
         search(element, parent, currentNode);                       //locate the the node which will be the parent of the node to be inserted
+
+        if (parent == NULL)                                         //if the parent is null (tree is empty)
+        {
+            root = newNode;                                         //mark  the new node as root
+            return;                                                 //exit
+        }
+        if (element < parent->info)                               //if the value in the data field of the new node is less than that of the parent
+        {
+            parent->leftchild = newNode;                            //make the left child of the parent point to the new node
+        }
+        else if (element > parent->info)                          //if the value in the data field of the new node is greater than of the parent
+        {
+            parent->rightchild = newNode;                           //make the right child of the parent point to the new node
+        }
     }
+
 };
 
 int main()
