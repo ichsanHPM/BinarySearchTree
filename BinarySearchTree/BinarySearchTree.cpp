9ede6389debba3a90b26f97e9200a78d220afbda
ichsanHPM <ichsan.helmi.ft22@mail.umy.ac.id>
2023-07-02 18:26:17 +0700

    case 4

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -151,7 +151,11 @@ int main()
             obj.preorder(obj.ROOT);
             break;
         }
-
+        case '4':
+        {
+            obj.postorder(obj.ROOT);
+            break;
+        }
         }
     }
 }
